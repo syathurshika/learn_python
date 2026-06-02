c3daf5614d8d90265e7b1eaa5a0d1e3b0bca6568
syathurshika <syathurshika2006@gmail.com>
2026-06-02 21:23:13 +0530

    Add day2.py for basic arithmetic operations with user input

---
+++ day2.py
@@ -0,0 +1,8 @@
+name=input("Enter your name:")
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+
+print(name,"your sum is", num1+num2)
+print(name, "your difference is" , num1-num2)
+print(name, "your product is", num1*num2)
+print(name, "your quotient is", num1/num2)
