cb12681068793669c89abb617b4ae6c4ca942b76
syathurshika <syathurshika2006@gmail.com>
2026-06-02 21:24:17 +0530

    Add day2hw.py to calculate and display BMI based on user input

---
+++ day2hw.py
@@ -0,0 +1,7 @@
+name=input("Enter Your Name:")
+weight=float(input("Enter Your Weight in kgs:"))
+height=float(input("Enter Your Height in metres:"))
+
+bmi=weight/(height * height)
+
+print(name,", your BMI is",bmi)
