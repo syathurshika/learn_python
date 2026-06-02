161e5b7b973d869679f9cda6856e445c00bd014e
syathurshika <syathurshika2006@gmail.com>
2026-06-02 21:20:50 +0530

    Add initial version of day1.py with personal information and print statements

---
+++ day1.py
@@ -0,0 +1,12 @@
+name="Yathurshika"
+age=20
+fav_food="hoppers"
+height=5.4
+is_student=True
+print("My name is", name)
+print("My age is" , age)
+print("My favourite food is" ,fav_food)
+print("My height is" , height)
+print("I am a student:", is_student)
+
+name={"name":"Yathurshika", "Age":20, "Score":89}
