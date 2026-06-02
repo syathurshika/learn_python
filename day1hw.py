1da6bf1c7fda20dc3e221e0ff3db0a91835c7c82
syathurshika <syathurshika2006@gmail.com>
2026-06-02 21:22:12 +0530

    Add day1hw.py with movie details and type checks

---
+++ day1hw.py
@@ -0,0 +1,13 @@
+title="Sirai"
+year=2025
+rating=9.5
+is_favourite=True
+
+print(title, "is a movie which is released in the year",year,".")
+print("My rating for the movie is",rating, "out of 10.")
+print("Favourite:",is_favourite)
+
+print(type(title))
+print(type(year))
+print(type(rating))
+print(type(is_favourite))
