11d69400bfe5a17d4cb52a4c94c4837581b48de0
syathurshika <syathurshika2006@gmail.com>
2026-06-03 20:10:06 +0530

    Add day4hw.py for number evaluation based on user input

---
+++ day4hw.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number between 1 and 100:"))
+
+if num<50:
+    print("Too low")
+elif num>80:
+    print("Too high")
+elif num!=75:
+        print("Just right")
+else:
+    print("Perfect!")
