91736c87736687615f749761d17626d50a169f67
syathurshika <syathurshika2006@gmail.com>
2026-06-03 20:17:03 +0530

    Add day5hw.py for printing even numbers from 1 to 50 using different methods

---
+++ day5hw.py
@@ -0,0 +1,16 @@
+#part1
+for i in range(2,51,2):
+    print(i)
+
+#part2
+count=2
+while count<51:
+    print(count)
+    count+=2
+
+#bonus
+for y in range(1,51):
+    if y%2==0:
+        print(y)
+        continue
+    
