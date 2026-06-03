d7115c5225a363c3b690da2da6ffa6fdf6fe4f54
syathurshika <syathurshika2006@gmail.com>
2026-06-03 20:12:14 +0530

    Refactor output messages in day4hw.py to use f-strings for improved readability

--- day4hw.py
+++ day4hw.py
@@ -1,10 +1,10 @@
 num=int(input("Enter a number between 1 and 100:"))
 
 if num<50:
-    print("Too low")
+    print(f"{num} is too low")
 elif num>80:
-    print("Too high")
+    print(f"{num} is too high")
 elif num!=75:
-        print("Just right")
+        print(f"{num} is just right")
 else:
-    print("Perfect!")
+    print(f"{num} is perfect!")
