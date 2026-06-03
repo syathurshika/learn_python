f8646cfb5df55e616fa05ccdb09d384e750be9fc
syathurshika <syathurshika2006@gmail.com>
2026-06-03 09:47:48 +0530

    Refactor print statements in day1.py for improved readability using f-strings

--- day1.py
+++ day1.py
@@ -3,10 +3,10 @@ age=20
 fav_food="hoppers"
 height=5.4
 is_student=True
-print("My name is", name)
-print("My age is" , age)
-print("My favourite food is" ,fav_food)
-print("My height is" , height)
-print("I am a student:", is_student)
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My favourite food is {fav_food}")
+print(f"My height is {height}")
+print(f"I am a student: {is_student}")
 
 name={"name":"Yathurshika", "Age":20, "Score":89}
