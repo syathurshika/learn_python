ca17f37ea179507806b1cbb62b1b19aff14fa358
syathurshika <syathurshika2006@gmail.com>
2026-06-06 21:50:04 +0530

    Add BMI calculation functionality and create new day8hw.py file

--- day8.py
+++ day8.py
@@ -18,3 +18,24 @@ mul(a,b)
 div(a,b)
 
 # task2
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    bmi = round(bmi, 2)
+    return bmi
+calculate_bmi(weight, height)
+
+bmi = calculate_bmi(weight, height)
+
+if bmi < 18.5:
+    bmi_category = "Underweight"
+elif 18.5 <= bmi < 24.9:
+    bmi_category = "Normal weight"
+elif 25 <= bmi < 29.9:
+    bmi_category = "Overweight"
+else:
+    bmi_category = "Obese"
+
+print(f"Your BMI is {bmi} and your category is {bmi_category}.")
